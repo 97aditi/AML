7eebc2ec9babd6074bd1d9a274ecca51debd12b6
Saksham Soni <saksham9315soni@gmail.com>
2018-01-26 12:02:40 +0530

    backprop basic structure

--- assignment1/neural_net.py
+++ assignment1/neural_net.py
@@ -1,28 +1,41 @@
 import numpy as np
 
 class Layer:
-	def __init__(self, units, units_prev, act)
+	def __init__(self, units_prev, units, act = 'sigmoid')
 		self.act = act         #weights, bias, z
 		self.inp = np.zeros((units_prev,1))
 		self.units = units
 		self.units_in_prev = units_prev
-		self.weights = np.randn(units, units_prev)   #Try xavier initialisation                    
-		self.bias = np.zeros((units,1))
-		self.z = np.zeros((units, 1)) #z is the activation of neurons in the layer
-		self.grad = None
+		self.weights = np.random.randn(units_prev, units)   #Try xavier initialisation                    
+		self.bias = np.random.randn(units,1)
+		self.z = np.zeros((units, 1)) # z = (w^l)T a^l-1 + b^l
+		self.gradW = None
+		self.gradb = None
 
 	def forward(self, inp):
 		self.inp = inp
-		self.z = self.activate(np.matmul(self.weights, self.inp) + self.bias)
-		return self.z
+		self.z = np.matmul(self.weights.T, self.inp) + self.bias
+		return self.activate(self.z)
 
-	def backprop(self, error):
-
+	def backprop(self, error, rate):
+		self.gradb = error
+		self.gradW = np.matmul(self.inp, self.z.T)
+		self.bias = self.bias - rate*self.gradb
+		self.weights = self.weights - rate*self.gradW
+		delta = np.multiply(np.matmul(self.weights, error), self.act_deriv(self.z))
 		return delta
 
 	def activate(self, x):
-		if(self.act == "sigmoid")
+		if(self.act == "sigmoid"):
 			return sigmoid(x)
+		if(self.act == 'softmax'):
+			return softmax(x)
+
+	def act_deriv(self,x):
+		if(self.act == 'sigmoid'):
+			return sig_deriv(x)
+		if(self.act == 'softmax'):
+			return smax_deriv(x)
 
 	def sigmoid(x):
 		return np.divide(1,np.add(1,np.exp(x)))
@@ -32,34 +45,42 @@ class Layer:
 		return np.multiply(sigmoid(x),(1-sigmoid(x)))
 
 	def softmax(x):
+		a0 = max(x, 0)
+		den = np.sum(np.exp(x-a0),0)
+		a = np.divide(np.exp(x-a0), den)
+		return a
 
 class NeuralNetwork:
-	def __init__(self, hidlayers, n_inputs, n_outputs, cost = 'crossent' , layers = None):
+	def __init__(self, hidlayers, n_inputs, n_outputs, cost = 'crossent' , layers = None, rate = 0.01):
 		self.hidlayers = hidlayers
 		self.n_inputs = n_inputs
 		self.n_outputs = n_outputs
 		self.layers = layers
-
+		self.rate = rate
+		self.cost = cost
              
-	def forwardpass(self, inputarr):
-		out = inputarr
+	def forwardPass(self, inputarr):
+		out = inputarr # should be D x N
 		for lr in self.layers:
 			out = lr.forward(out)
 		return out
 
-	def costfunc(cost, expected):
-		if (cost =="crossent")
-			error = np.negative(np.sum(np.multiply(expected, np.log(self.layers[hidlayers-1].z))))
+	def costFunc(self, trueval):
+		if (self.cost =="crossent")
+			error = np.negative(np.sum(np.multiply(trueval, np.log(self.layers[hidlayers-1].z))))
 
 
-	def backprop(self, expected):	
-		delta[hidlayers-1] = 		#last layer error						
-		for i in range(hidlayers-2, 0, -1)
-			delta[i] = np.multiply(np.matmul(np.transpose(self.layers[i+1].weights), delta[i+1]), deriv(act, self.layers[i].z))
+	def backProp(self, trueval):
+		error = self.costFunc(trueval)
+		for lr in reversed(layers):
+			delta = lr.backprop(error, self.rate)
+			error = delta
+
 
 ## Hyper-parameters
 D = 784 # input dimension
 m = 9 # no of classes
+alpha = 0.01
 
-neurons = [Layer(D,1024), Layer(1024,512), Layer(512,256), Layer(256, 100), Layer(100,m)]
-NN = NeuralNetwork(4, D, m, layers = neurons)
+neurons = [Layer(D,1024, 'sigmoid'), Layer(1024,512, 'sigmoid'), Layer(512,256, 'sigmoid'), Layer(256, 100, 'sigmoid'), Layer(100,m, 'softmax')]
+NN = NeuralNetwork(4, D, m, layers = neurons, rate = alpha)
