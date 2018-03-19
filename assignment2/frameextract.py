19ca9c49f60b006a5118e3fad06b2d237e73da69
97aditi <aditidps07@gmail.com>
2018-03-19 22:07:17 +0530

    Update frameextract.py

--- assignment2/frameextract.py
+++ assignment2/frameextract.py
@@ -1,9 +1,10 @@
 import cv2
-vidcap = cv2.VideoCapture('name of video')
+vidcap = cv2.VideoCapture('\youtubevids\maheshwari\m_vid1.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
 while success:
   success,image = vidcap.read()
-  cv2.imwrite("frame%d.jpg" % count, image)
+  img = cv2.resize(image, (256,256))
+  cv2.imwrite("\youtubevids\mvid1frames\mr%d.jpg" % count, img)
   count += 1
